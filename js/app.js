48192239b59c6eccddc4535d7691327424daacd3
AkhiSharmin <akhisharmin100@gmail.com>
2023-09-18 06:51:56 +0600

    added spiner

--- js/app.js
+++ js/app.js
@@ -42,13 +42,28 @@ const displayPhone = phones => {
         `;
         phonesContainer.appendChild(phoneDiv);
     });
+    //stop spinier loader
+    toggleSpinner(false)
+
 };
 
 
 document.getElementById('btn-search').addEventListener('click', function () {
+    // start loader 
+    toggleSpinner(true)
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhones(searchText);
 })
+
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none')
+    } else {
+        loaderSection.classList.add('d-none')
+    }
+}
 
 // loadPhones();
